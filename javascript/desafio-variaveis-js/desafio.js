f4917c7ada421f27ef7d89515cfc1214976197ac
Salomão Campagna <campagnasalomao@gmail.com>
2026-05-05 14:12:59 -0300

    Add files via upload

---
+++ javascript/desafio-variaveis-js/desafio.js
@@ -0,0 +1,87 @@
+/*
+====================================
+DESAFIO DE VARIÁVEIS EM JAVASCRIPT
+====================================
+
+Complete o código abaixo.
+
+Você deverá usar:
+
+- variáveis
+- declaração
+- inicialização
+- escopo
+*/
+
+
+// =============================
+// 1 - ARMAZENANDO DADOS
+// =============================
+
+// TODO:
+// crie uma variável chamada nomeAluno
+let nomeAluno = "Carrisa Véliz"
+
+// TODO:
+// crie duas variáveis chamadas nota1 e nota2
+
+ let nota1 = 8.5
+ let nota2 = 9.0
+
+// =============================
+// 2 - DECLARAÇÃO
+// =============================
+ 
+ 
+// TODO:
+// declare uma variável chamada media
+let media;
+
+// =============================
+// 3 - INICIALIZAÇÃO
+// =============================
+
+// TODO:
+// calcule a média das notas
+
+// formula:
+// (nota1 + nota2) / 2
+
+media = (nota1 + nota2) / 2;
+
+// =============================
+// 4 - ESCOPO
+// =============================
+
+// Regra:
+// média >= 6 aprovado
+// média < 6 reprovado
+  let resultado
+
+if (media >= 6) {
+    resultado = "Aprovado"
+} else {
+    resultado = "Reprovado"
+}    
+
+
+    // TODO:
+    // crie a variável resultado
+    // use a média para definir se é Aprovado ou Reprovado
+
+
+
+
+
+// =============================
+// 5 - SAÍDA DO PROGRAMA
+// =============================
+
+console.log("Aluno:", nomeAluno)
+console.log("Nota 1:", nota1)
+console.log("Nota 2:", nota2)
+
+console.log("Média:", media)
+console.log("Resultado:", resultado)   
+// TODO:
+// mostrar resultado
